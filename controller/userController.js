0cf332281f4197ef2fade148767cf1e91ccc553d
Satya <89769507+SatyaRy@users.noreply.github.com>
2025-05-29 11:34:08 +0700

    update: authentication create account and login

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,24 @@
-import {createUserAccount  } from "../service/userService.js"
+import {createAccountService,loginAccountService } from "../service/userService.js"
 export const createAccount = async (req,res)=>{
     try {
         const {email,password,name} = req.body
-        const response  = await createUserAccount(email,password,name);
+        const response  = await createAccountService (email,password,name);
         console.log(response.message)
+        res.status(response.status).json({
+            data: {
+                message: response.message,
+                token: response.token
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const loginUserAccount = async (req,res)=>{
+    try {
+        const {email,password} = req.body 
+        const response = await loginAccountService(email,password)
         res.status(response.status).json({
             data: {
                 message: response.message,
