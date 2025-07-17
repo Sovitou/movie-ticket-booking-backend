eecf6f00b768ba731ab5055670525051f59d76d2
Sovitou Khem <khemsovitou@gmail.com>
2025-07-17 23:23:21 +0700

    "feat: Implement Admin User Management APIs"

---
+++ src/controllers/admin.controller.js
@@ -0,0 +1,44 @@
+import * as adminService from "../services/admin.service.js";
+
+export async function getUsers(req, res, next) {
+  try {
+    const result = await adminService.getUsers();
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getUserById(req, res, next) {
+  const { id } = req.params;
+  const parsedId = parseInt(id);
+  try {
+    const result = await adminService.getUserById(parsedId);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function deleteUserById(req, res, next) {
+  const { id } = req.params;
+  const parsedId = parseInt(id);
+  try {
+    const result = await adminService.deleteUserById(parsedId);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function updateUserById(req, res, next) {
+  const { id } = req.params;
+  const parsedId = parseInt(id);
+  const data = req.body;
+  try {
+    const result = await adminService.updateUserById(parsedId, data);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
