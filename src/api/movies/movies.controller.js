e12d37f8a03fee96790072df86b5cc7d0007c4ce
Sovitou Khem <khemsovitou@gmail.com>
2025-07-21 22:22:49 +0700

    chore: Update .gitignore and add movies route
    - Added .cursorrules to .gitignore.
    - Integrated moviesRoutes into the main routing file.

---
+++ src/api/movies/movies.controller.js
@@ -0,0 +1,55 @@
+import {
+  MovieCreateSchema,
+  MovieUpdateSchema,
+} from "../../models/movie.model.js";
+import * as movieService from "./movies.service.js";
+
+export async function createMovie(req, res, next) {
+  try {
+    const data = MovieCreateSchema.parse(req.body);
+    const result = await movieService.createMovie(data);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function updateMovie(req, res, next) {
+  try {
+    const { id } = req.params;
+    const data = MovieUpdateSchema.parse(req.body);
+    const result = await movieService.updateMovie(id, data);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function deleteMovie(req, res, next) {
+  try {
+    const { id } = req.params;
+    const result = await movieService.deleteMovie(id);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getMovies(req, res, next) {
+  try {
+    const result = await movieService.getMovies();
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getMovieById(req, res, next) {
+  try {
+    const { id } = req.params;
+    const result = await movieService.getMovieById(id);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "../api/auth/auth.routes.js";
 import profileRoutes from "../api/users/profile.routes.js";
 import adminRoutes from "../api/admin/admin.routes.js";
+import moviesRoutes from "../api/movies/movies.route.js";
 
 const route = express.Router();
 
@@ -9,5 +10,6 @@ const route = express.Router();
 route.use("/auth", authRoutes); //authentication
 route.use("/profile", profileRoutes); //profile
 route.use("/admin", adminRoutes); //admin
+route.use("/movies", moviesRoutes); //movies
 
 export default route;
